c3fcaaeebd4b1f5955de4cefc94a8e2adc9d5afd
Vannie2023 <122510489+Vannie2023@users.noreply.github.com>
2024-11-28 04:30:27 -0500

    Modified

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,16 +6,16 @@ const Navbar = () => {
     <div className='navbar'>
      <div className="container">
         <div className="logo">
-            <span className='text'> fiverr</span>
+            <span className='text'> FoodyEco</span>
             <span className='dot'>.</span>
         </div>
         <div className="links">
-            <span>Fiverr Business</span>
+            <span>Business Pro</span>
             <span>Explore</span>
             <span>English</span>
             <span>Sign in</span>
             <span>Become a Seller</span>
-            <button>Join</button>
+            <button>Sign Up</button>
         </div>
      </div>
     </div>
