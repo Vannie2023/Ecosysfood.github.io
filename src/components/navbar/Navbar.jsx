6f611edf8e7de9f53f82d22a2c59416f4bcfb352
Vannie2023 <122510489+Vannie2023@users.noreply.github.com>
2024-11-28 02:12:59 -0500

    Navbar added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,15 @@ import "./Navbar.scss"
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+     <div className="container">
+        <div className="logo">
+            <span className='text'> fiverr</span>
+            <span>.</span>
+        </div>
+        <div className="links"></div>
+     </div>
+    </div>
   )
 }
 
