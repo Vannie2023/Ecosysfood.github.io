442d4459e1149fe016c52a4095de13fc2ade60ae
Vannie2023 <122510489+Vannie2023@users.noreply.github.com>
2025-01-26 20:52:08 -0500

    New files added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState} from 'react';
+import { Link } from "react-router-dom";
 import "./Navbar.scss"
 
 const Navbar = () => {
@@ -28,8 +29,10 @@ const Navbar = () => {
     <div className={active ? "navbar active" :"navbar"}>
      <div className="container">
         <div className="logo">
+          <Link to="/" className="link">
             <span className='text'> FoodyEco</span>
             <span className='dot'>.</span>
+            </Link>
         </div>
         <div className="links">
             <span>Business Pro</span>
@@ -46,13 +49,13 @@ const Navbar = () => {
                   {
                     currentUser?.isSeller && (
                       <>
-                      <span>Gigs</span>
-                      <span>Add New Gig</span>
+                      <Link className="link" to="/mygigs">Gigs</Link>
+                      <Link className="link" to="/add">Add New Gig</Link>
                       </>
                     )}
-                    <span>Orders</span>
-                    <span>Messages</span>
-                    <span>Logout</span>
+                    <Link className="link" to="/orders">Orders</Link>
+                    <Link className="link" to="/messages">Messages</Link>
+                    <Link className="link" to="/">Logout</Link>
               </div>}
               </div>    
             )}
