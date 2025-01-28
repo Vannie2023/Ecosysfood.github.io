56df449075f4fe6a31b855d4bdda8cd18476406c
Vannie2023 <122510489+Vannie2023@users.noreply.github.com>
2025-01-27 22:42:32 -0500

    Added components

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState} from 'react';
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import "./Navbar.scss"
 
 const Navbar = () => {
 
   const [active, setActive] = useState(false);
   const [open, setOpen] = useState(false);
+
+  const {pathname} = useLocation()
 
   const isActive = ()=>{
     window.scrollY > 0 ? setActive(true) : setActive(false);
@@ -26,7 +29,7 @@ const Navbar = () => {
   }
 
   return (
-    <div className={active ? "navbar active" :"navbar"}>
+    <div className={active || pathname !== "/" ? "navbar active" :"navbar"}>
      <div className="container">
         <div className="logo">
           <Link to="/" className="link">
@@ -61,14 +64,37 @@ const Navbar = () => {
             )}
         </div>
         </div>
-        {active && (
+        {(active || pathname !== "/") && (
          <>
           <hr />
           <div className="menu">
-           <span>Test</span>
-           <span>Test</span>
-           <span>Test</span>
-           <span>Test</span>
+           <Link className="link" to="/">
+           Graphic & Design
+           </Link>
+           <Link className="link" to="/">
+           Video & Animation
+           </Link>
+           <Link className="link" to="/">
+           Writing & Translation
+           </Link>
+           <Link className="link" to="/">
+           AI Services
+           </Link>
+           <Link className="link" to="/">
+           Digital Marketing
+           </Link>
+           <Link className="link" to="/">
+           Music & Audio
+           </Link>
+           <Link className="link" to="/">
+           Programming & Tech
+           </Link>
+           <Link className="link" to="/">
+           Business
+           </Link>
+           <Link className="link" to="/">
+           Lifestyle
+           </Link>
          </div>
        </>
       )}
