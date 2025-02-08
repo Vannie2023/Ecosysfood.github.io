a6d9769e73a30c5625f1a211108aa1b79df2ccae
Vannie2023 <122510489+Vannie2023@users.noreply.github.com>
2025-02-07 21:29:32 -0500

    Added Slider

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React from 'react';
 import Featured from "../../components/Featured/Featured";
-import "./Home.scss"
+import "./Home.scss";
+import Slide from "../../components/slide/Slide";
+import CatCard from "../../components/catCard/CatCard";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
+import { cards, projects } from "../../data";
 
 const Home = () => {
   return (
     <div className='home'>
       <Featured />
+      <TrustedBy />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map((card) => (
+          <CatCard key={card.id} card={card} />
+        ))}
+      </Slide>
     </div>
-  )
-}
+  );
+};
 
 export default Home
