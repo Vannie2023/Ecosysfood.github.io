70924455e94ea2746820a49ae64a02e9a090024d
Vannie2023 <122510489+Vannie2023@users.noreply.github.com>
2025-02-07 19:43:17 -0500

    Updated service sections

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import "./Featured.scss"
 
-const Featured = () => {
+
+/*const Featured = () => {
+  */
+  function Featured() {
   return (
-    <div className='featured'>
+    <div className="featured">
         <div className="container">
         <div className="left">
-            <h1>Find the perfect freelance services for your business</h1>
+            <h1>
+              Find the perfect <i>freelance</i> services for your business
+              </h1>
                <div className="search">
                  <div className="searchInput">
-                    <img src="" alt="" />
+                    <img src="./img/search.png" alt="" />
                     <input type="text" placeholder='Try "building mobile app"' />
                 </div>
                 <button>Search</button>
@@ -20,14 +25,14 @@ const Featured = () => {
                 <button>WordPress</button>
                 <button>Logo Design</button>
                 <button>AI Services</button>
-
             </div>
         </div>
-        <div className="right"></div>
-
+        <div className="right">
+          <img src="./img/man.png" alt="" />
+        </div>
         </div>
     </div>
-  )
+  );
 }
 
-export default Featured
+export default Featured;
